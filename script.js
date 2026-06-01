d2f58b349f50bc056c250177ef544f6744155d33
madudonald36-prog <madudonald36@gmail.com>
2026-06-01 17:42:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showMessage() {
   document.getElementById("message").innerHTML =
-    "Happy Birthday Mum! Thank you for loving me, supporting me, and always being there for me ❤️";
+    "Mum, you are loved more than words can ever explain. Happy Birthday, my queen 👑❤️";
 }
