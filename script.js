3881b0dc77a5ec541861b55deb895ec9836753f9
madudonald36-prog <madudonald36@gmail.com>
2026-06-01 18:24:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function showMessage() {
   const message = document.getElementById("message");
 
   message.innerHTML =
-    "✨ Surprise! ✨<br>Mum, you are my greatest blessing, my first teacher, my safe place, and my forever hero.<br><br>Happy Birthday, my beautiful queen 👑❤️🦋";
+    " ✨<br>Mum, you are my greatest blessing, my first teacher, my safe place, and my forever hero.<br><br>Happy Birthday, my beautiful queen 👑❤️🦋";
 
   message.classList.remove("magic-message");
 
